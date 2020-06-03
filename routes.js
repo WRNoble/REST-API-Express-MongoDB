e7800f0c30950bd1e2b75d250dfeca9cbf73a6cf
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-06-03 11:30:45 -0400

    more routes done

--- routes.js
+++ routes.js
@@ -15,10 +15,51 @@ router.post("/", function(req, res){
 })
 
 //GET /questions/"id"
-router.get("/:id", function(req, res){
+router.get("/:qID", function(req, res){
     res.json({
-        response: "You sent me a GET request for a specific ID " + req.params.id
+        response: "You sent me a GET request for a specific ID-" + req.params.qID
     });
 })
+
+//POST /questions/:id/answers
+router.post("/:qID/answers", function(req, res){
+    res.json({
+        response: "You sent me a POST request to /answers",
+        questionId: req.params.qID,
+        body: req.body
+    });
+})
+
+//PUT /question/:qID/answers/:aID
+router.put("/:qID/answers/:aID", function(req, res){
+    res.json({
+        response: "You sent me a PUT request to /answers",
+        questionId: req.params.qID,
+        answerId: req.params.aID,
+        body: req.body
+    });
+})
+
+//DELETE /question/:qID/answers/:aID
+router.delete("/:qID/answers/:aID", function(req, res){
+    res.json({
+        response: "You sent me a DELETE request to /answers",
+        questionId: req.params.qID,
+        answerId: req.params.aID,
+    });
+})
+
+//POST /question/:qID/answers/:aID/vote-up
+//POST /question/:qID/answers/:aID/vote-down
+//vote 
+router.post("/:qID/answers/:aID/vote-:dir", function(req, res){
+    res.json({
+        response: "You sent me a POST request to /vote-" + req.params.dir,
+        questionId: req.params.qID,
+        answerId: req.params.aID,
+        vote: req.params.dir
+    });
+})
+
 
 module.exports = router;
